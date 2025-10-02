a1fc71f4c6ecf50decb85059265703838437e3d6
tomimastro069 <tomimastro069@gmail.com>
2025-10-01 23:39:24 -0300

    Resolviendo conflictos tras aplicar stash

--- Backend/PruebaCargaImagenesSQL/src/main/java/org/example/repository/ImagenRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.repository;
-
-import org.example.Entity.Imagen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImagenRepository extends JpaRepository<Imagen, Integer> {}
--- Backend/PruebaCargaImagenesSQL/src/main/java/org/example/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-
-import org.example.Entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {}

2e39b22f92a92f8c32a8ecec1a20694c6bc06583
CristianK25 <krahulikcristian@gmail.com>
2025-10-20 12:47:14 -0300

    actualizo usuario repo y service

--- Backend/BackendE_Commerce/src/main/java/org/springej/backende_commerce/repository/UsuarioRepository.java
+++ Backend/BackendE_Commerce/src/main/java/org/springej/backende_commerce/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package org.springej.backende_commerce.repository;
 
 import org.springej.backende_commerce.entity.Usuario;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByNombre(String nombre);
 
     Optional<Usuario> findByEmail(String email);
+
+    @EntityGraph(attributePaths = "roles")
+    Optional<Usuario> findWithRolesByEmail(String email);
 }
